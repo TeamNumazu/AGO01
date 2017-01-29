207129a0bf37acaa6f39171b1eec8951b07b877c
ne-peer <mileage0110@gmail.com>
2017-01-29 15:15:11 +0900

    add listview example

--- src/app/src/main/java/com/example/silve/ago01/MainActivity.java
+++ src/app/src/main/java/com/example/silve/ago01/MainActivity.java
@@ -12,14 +12,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    ListView listView;
+
+    private static final String[] foods = {
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+            "たまご", "とうふ", "バナナ", "レタス", "エリンギ", "巨乳",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        listView.setAdapter(arrayAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
