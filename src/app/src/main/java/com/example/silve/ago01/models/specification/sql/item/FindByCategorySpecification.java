7762257e5dfbe0551e8ad1082cc5338b147e42ce
f5developper <f5developper@gmail.com>
2017-02-24 08:25:15 +0900

    商品をカテゴリで取得するクラスを作成

---
+++ src/app/src/main/java/com/example/silve/ago01/models/specification/sql/item/FindByCategorySpecification.java
@@ -0,0 +1,26 @@
+package com.example.silve.ago01.models.specification.sql.item;
+
+import com.example.silve.ago01.models.AgoContract;
+import com.example.silve.ago01.models.specification.sql.SqlSpecification;
+
+/**
+ * Created by silve on 2017/02/24.
+ */
+
+public class FindByCategorySpecification implements SqlSpecification {
+
+    protected int categoryId = -1;
+
+    public FindByCategorySpecification(int id) {
+        categoryId = id;
+    }
+
+    @Override
+    public String toSqlQuery() {
+
+        return String.format("SELECT * FROM ITEM WHERE %1$S WHERE " +
+                AgoContract.Item.COLUMN_NAME_CATEGORYID + "= %2$S",
+                AgoContract.Item.TABLE_NAME
+        );
+    }
+}
