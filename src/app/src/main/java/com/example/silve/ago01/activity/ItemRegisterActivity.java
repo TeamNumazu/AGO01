070024a49b98ffa1e105522de56b198a17ac0f81
f5developper <f5developper@gmail.com>
2017-03-12 18:42:52 +0900

    カメラ画像の表示処理を追加

--- src/app/src/main/java/com/example/silve/ago01/activity/ItemRegisterActivity.java
+++ src/app/src/main/java/com/example/silve/ago01/activity/ItemRegisterActivity.java
@@ -1,9 +1,13 @@
 package com.example.silve.ago01.activity;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.MediaScannerConnection;
@@ -13,7 +17,9 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -32,6 +38,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import android.database.Cursor;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ItemRegisterActivity extends AppCompatActivity {
 
@@ -78,12 +86,14 @@ public class ItemRegisterActivity extends AppCompatActivity {
 
     static protected int REQUEST_CODE_CAMERA  = 0x00000001;
     static protected int REQUEST_CODE_GALLERY = 0x00000002;
+    static protected int REQUEST_USE_CAMERA_AND_STORAGE = 0x00000003;
 
 
     private Bitmap bitmap;
     private Uri bitmapUri;
 
     public void onClickCameraButton(View v) {
+//        wakeupCamera(); // カメラ起動
         //選択項目を準備する。
         String[] str_items = {"カメラで撮影", "ギャラリーから選択", "キャンセル"};
         new AlertDialog.Builder(this)
@@ -111,11 +121,22 @@ public class ItemRegisterActivity extends AppCompatActivity {
      * カメラ起動するもよう
      */
     protected void wakeupCamera(){
-        File mediaStorageDir = new File(
-                Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_PICTURES
-                ), "PictureSaveDir"
-        );
+        File mediaStorageDir = null;
+
+
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            //RUNTIME PERMISSION Android M
+            if(PackageManager.PERMISSION_GRANTED== ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) &&
+                    PackageManager.PERMISSION_GRANTED== ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)){
+                mediaStorageDir = new File(
+                        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"IMG"
+                );
+            }else{
+                requestCameraPermission(this);
+                return;
+            }
+
+        }
 
         if(! mediaStorageDir.exists() & ! mediaStorageDir.mkdirs()) {
             Log.d("TAG", "Failed to create directory");
@@ -124,12 +145,33 @@ public class ItemRegisterActivity extends AppCompatActivity {
 
         String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
         File bitmapFile = new File(mediaStorageDir.getPath() + File.separator + timeStamp + ".JPG");
-        Uri bitmapUri = Uri.fromFile(bitmapFile);
+
+        bitmapUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", bitmapFile);
 
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         i.putExtra(MediaStore.EXTRA_OUTPUT, bitmapUri);
         startActivityForResult(i, REQUEST_CODE_CAMERA);
     }
+
+    private static void requestCameraPermission(final Context context){
+        if(ActivityCompat.shouldShowRequestPermissionRationale((Activity)context,Manifest.permission.WRITE_EXTERNAL_STORAGE)||
+                ActivityCompat.shouldShowRequestPermissionRationale((Activity)context,Manifest.permission.CAMERA)){
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA},
+                    REQUEST_USE_CAMERA_AND_STORAGE);
+
+        }else {
+            // permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions((Activity)context,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.CAMERA},
+                    REQUEST_USE_CAMERA_AND_STORAGE);
+        }
+    }
+
 
     /**
      * ギャラリーを起動だ！
@@ -139,6 +181,27 @@ public class ItemRegisterActivity extends AppCompatActivity {
         i.setType("image/*");
         i.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(i, REQUEST_CODE_GALLERY);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0x00000003: {
+                if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this,
+                            "成功やで",
+                            Toast.LENGTH_SHORT).show();
+                    wakeupCamera();
+
+                } else {
+                    Toast.makeText(this,
+                            "失敗やで",
+                            Toast.LENGTH_SHORT).show();
+                    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                }
+                return;
+            }
+        }
     }
 
 
@@ -182,7 +245,10 @@ public class ItemRegisterActivity extends AppCompatActivity {
                     break;
             }
             //こいつは受け取った写真のviewになる。
-//            preview.setImageBitmap(bitmap);
+            ImageView itemView = (ImageView) findViewById(R.id.item_view);
+            itemView.setImageURI(bitmapUri);
+//            itemView.setImageBitmap(bitmap);
+
         }
     }
 
