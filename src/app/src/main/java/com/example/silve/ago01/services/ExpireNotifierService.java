cd9096a1dbad8b4bc4e7c3cdfc9a34199dee046c
agostick <k-iwashiro@intworks.co.jp>
2017-03-22 20:01:42 +0900

    [update] 賞味期限切れの比較を実装

--- src/app/src/main/java/com/example/silve/ago01/services/ExpireNotifierService.java
+++ src/app/src/main/java/com/example/silve/ago01/services/ExpireNotifierService.java
@@ -4,14 +4,17 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
-import android.widget.Toast;
 
 import com.example.silve.ago01.models.DataBaseHelper;
 import com.example.silve.ago01.models.entity.Item;
 import com.example.silve.ago01.models.repository.ItemRepository;
 import com.example.silve.ago01.models.specification.sql.item.FindAllSpecification;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,8 +24,10 @@ public class ExpireNotifierService extends Service {
 
     /**
      * 何分おきに実行するか
+     * <p>
+     * 30なら30分毎、最大59まで設定可。
      */
-    private static final int NOTIFIER_EXEC_PER = 2;
+    private static final int NOTIFIER_EXEC_PER = 1;
 
     /**
      * 初期化するだけ
@@ -37,10 +42,14 @@ public class ExpireNotifierService extends Service {
      */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (this.shouldTaskStart() == true) {
-            Toast.makeText(this, "AGO！", Toast.LENGTH_LONG).show();
-
+        if (this.shouldTaskStart() == false) {
+            // すべての商品
             List<Item> itemList = this.findItemAll(getApplicationContext());
+
+            // 有効期限の切れている商品だけを取得
+            List<Item> expiredItemList = this.getExpiredList(itemList);
+
+
         }
 
         return super.onStartCommand(intent, flags, startId);
@@ -61,21 +70,19 @@ public class ExpireNotifierService extends Service {
      *
      * @return boolean
      */
-    private boolean shouldTaskStart()
-    {
-        CharSequence textMinutes  = android.text.format.DateFormat.format("mm", Calendar.getInstance());
+    private boolean shouldTaskStart() {
+        CharSequence textMinutes = android.text.format.DateFormat.format("mm", Calendar.getInstance());
         int intMinutes = Integer.parseInt(textMinutes.toString());
 
         return (intMinutes % NOTIFIER_EXEC_PER) == 0;
     }
 
     /**
-     * 商品一覧をすべて取得
+     * 商品一覧をすべて取得（デバッグ用にPublicなので後で変える）
      *
      * @return
      */
-    public List<Item> findItemAll(Context context)
-    {
+    public List<Item> findItemAll(Context context) {
         // リポジトリ用意
         DataBaseHelper dbHelper = new DataBaseHelper(context);
         ItemRepository cRepository = new ItemRepository(dbHelper);
@@ -84,9 +91,48 @@ public class ExpireNotifierService extends Service {
         FindAllSpecification iAllSpec = new FindAllSpecification();
 
         // 取得実行
-        List<Item> itemList =  cRepository.query(iAllSpec);
+        List<Item> itemList = cRepository.query(iAllSpec);
 
         return itemList;
+    }
+
+    /**
+     * 有効期限が今日より前日の商品リストを取得します
+     *
+     * @param itemList
+     * @return
+     */
+    private List<Item> getExpiredList(List<Item> itemList) {
+        List<Item> expiredList = new ArrayList<>();
+
+        // 比較用日付フォーマット
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+
+        for (int i = 0, size = itemList.size(); i < size; i++) {
+            Item item = itemList.get(i);
+            String itemExpireAt = item.getExpiredAt();
+
+            if (itemExpireAt == null) {
+                continue;
+            }
+
+            try {
+                // 現在日付
+                Date now = new Date();
+                Date itemExpiredAt = format.parse(item.getExpiredAt());
+
+                // 比較
+                if (now.after(itemExpiredAt)) {
+                    // 今日より前日ならリストに追加
+                    expiredList.add(item);
+                }
+            } catch (ParseException ex) {
+                // ナイスキャッチ！
+            }
+
+        }
+
+        return expiredList;
     }
 
 }
