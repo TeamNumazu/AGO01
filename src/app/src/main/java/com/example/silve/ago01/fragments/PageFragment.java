1762a5867fa5f024491321725f96514612515844
agostick <k-iwashiro@intworks.co.jp>
2017-04-01 17:52:05 +0900

    [update] PagerにList表示するように

--- src/app/src/main/java/com/example/silve/ago01/fragments/PageFragment.java
+++ src/app/src/main/java/com/example/silve/ago01/fragments/PageFragment.java
@@ -1,15 +1,31 @@
 package com.example.silve.ago01.fragments;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.swipe.util.Attributes;
 import com.example.silve.ago01.R;
+import com.example.silve.ago01.models.DataBaseHelper;
+import com.example.silve.ago01.models.entity.Item;
+import com.example.silve.ago01.models.repository.ItemRepository;
+import com.example.silve.ago01.models.specification.sql.item.FindAllSpecification;
+import com.example.silve.ago01.views.ListViewAdapter;
+
+import java.util.List;
 
 public class PageFragment extends Fragment {
 
@@ -43,8 +59,8 @@ public class PageFragment extends Fragment {
         // Inflate the layout for this fragment
         int page = getArguments().getInt(ARG_PARAM, 0);
         View view = inflater.inflate(R.layout.fragment_page, container, false);
-        TextView vvv = (TextView)view.findViewById(R.id.flgTextView);
-        vvv.setText("Page" + page);
+
+        this.createListView(view);
 
         return view;
     }
@@ -74,5 +90,76 @@ public class PageFragment extends Fragment {
 
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
+    }
+
+
+    public void createListView(View view) {
+        /**
+         * データ取得
+         */
+        DataBaseHelper dbHelper = new DataBaseHelper(getActivity());
+        ItemRepository iRepository = new ItemRepository(dbHelper);
+
+        FindAllSpecification iAllSpec = new FindAllSpecification();
+        List<Item> itemList = iRepository.query(iAllSpec);
+
+        /**
+         * SwipeListViewをつくる
+         */
+        final ListView listView;
+        ListViewAdapter listAdapter;
+
+        listView = (ListView) view.findViewById(R.id.listview);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActivity().getActionBar();
+            if (actionBar != null) {
+                actionBar.setTitle("ListView");
+            }
+        }
+
+        listAdapter = new ListViewAdapter(getActivity(), itemList);
+        listView.setAdapter(listAdapter);
+        listAdapter.setMode(Attributes.Mode.Single);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ((SwipeLayout) (listView.getChildAt(position - listView.getFirstVisiblePosition()))).open(true);
+            }
+        });
+        listView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), "OnItemLongClickListener", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                Log.e("ListView", "onScrollStateChanged");
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("ListView", "onItemSelected:" + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.e("ListView", "onNothingSelected:");
+            }
+        });
     }
 }
