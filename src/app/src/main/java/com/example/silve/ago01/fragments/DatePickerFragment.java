9a6247ab2784a23f4c782d0da6652e3a64d69116
agostick <k-iwashiro@intworks.co.jp>
2017-04-10 20:05:39 +0900

    [fix] 商品期限の日付がずれるバグを修正

--- src/app/src/main/java/com/example/silve/ago01/fragments/DatePickerFragment.java
+++ src/app/src/main/java/com/example/silve/ago01/fragments/DatePickerFragment.java
@@ -1,22 +1,12 @@
 package com.example.silve.ago01.fragments;
 
-import android.content.Context;
-//import android.icu.util.Calendar;
 import java.util.Calendar;
-
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.silve.ago01.R;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.text.format.DateFormat;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -31,17 +21,18 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Create a new instance of TimePickerDialog and return it
-        return new DatePickerDialog(getActivity(), this, year, month,
-                day);
-
+        return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
         EditText editText = (EditText)getActivity().findViewById(R.id.expired_at);
-        editText.setText(year + "/" +month+ "/" + dayOfMonth);
-    }
 
+        // 月が1ヶ月ずれるので強引にfix
+        // 年末年始でもおかしな日付にはならんかった
+        int fixedMonth = month + 1;
+
+        editText.setText(year + "/" + fixedMonth + "/" + dayOfMonth);
+    }
 
 }
