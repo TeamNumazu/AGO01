1ede1be8abf3ca75d7e41bc4698377d03ff2b530
f5developper <f5developper@gmail.com>
2017-04-30 16:15:51 +0900

    画像表示できるように修正とフォーマッタ適用

--- src/app/src/main/java/com/example/silve/ago01/views/ListViewAdapter.java
+++ src/app/src/main/java/com/example/silve/ago01/views/ListViewAdapter.java
@@ -1,11 +1,19 @@
 package com.example.silve.ago01.views;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.daimajia.swipe.SimpleSwipeListener;
 import com.daimajia.swipe.SwipeLayout;
 import com.daimajia.swipe.adapters.BaseSwipeAdapter;
@@ -14,6 +22,9 @@ import com.example.silve.ago01.models.DataBaseHelper;
 import com.example.silve.ago01.models.entity.Item;
 import com.example.silve.ago01.models.repository.ItemRepository;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,7 +46,7 @@ public class ListViewAdapter extends BaseSwipeAdapter {
         initialize();
     }
 
-    public void initialize(){
+    public void initialize() {
         mIRepository = new ItemRepository(new DataBaseHelper(mContext));
 
     }
@@ -48,7 +59,7 @@ public class ListViewAdapter extends BaseSwipeAdapter {
     @Override
     public View generateView(int position, ViewGroup parent) {
         View v = LayoutInflater.from(mContext).inflate(R.layout.listview_item, null);
-        final SwipeLayout swipeLayout = (SwipeLayout)v.findViewById(getSwipeLayoutResourceId(position));
+        final SwipeLayout swipeLayout = (SwipeLayout) v.findViewById(getSwipeLayoutResourceId(position));
         swipeLayout.addSwipeListener(new SimpleSwipeListener() {
             @Override
             public void onOpen(SwipeLayout layout) {
@@ -72,10 +83,10 @@ public class ListViewAdapter extends BaseSwipeAdapter {
             // 削除おしたとき
             @Override
             public void onClick(View view) {
-                for(int i = 0 ; i < mItemList.size() ; i++){
+                for (int i = 0; i < mItemList.size(); i++) {
 
-                    if(mItemManger.isOpen(i)){
-                        Item item  = mItemList.get(i);
+                    if (mItemManger.isOpen(i)) {
+                        Item item = mItemList.get(i);
                         mIRepository.remove(item);
                         mItemList.remove(item);
                         break;
@@ -101,22 +112,45 @@ public class ListViewAdapter extends BaseSwipeAdapter {
         Item item = mItemList.get(position);
 
         // インデックス
-        TextView itemIndex = (TextView)convertView.findViewById(R.id.position_index);
+        TextView itemIndex = (TextView) convertView.findViewById(R.id.position_index);
         String index = String.valueOf(position + 1);
         itemIndex.setText(index);
 
         // 商品名
-        TextView name = (TextView)convertView.findViewById(R.id.position_name);
+        TextView name = (TextView) convertView.findViewById(R.id.position_name);
         name.setText(item.getItemName());
 
         // 個数
-        TextView quantity = (TextView)convertView.findViewById(R.id.position_quantity);
+        TextView quantity = (TextView) convertView.findViewById(R.id.position_quantity);
         quantity.setText(String.valueOf(item.getNumber()));
 
         // 期限
         TextView expire = (TextView) convertView.findViewById(R.id.position_expire);
         expire.setText(item.getExpiredAt());
 
+        ImageView itemView = (ImageView) convertView.findViewById(R.id.item_row_view);
+
+        Uri bitmapUrl = null;
+
+        try {
+//            File bitmapFile = new File(item.getItemImage());
+//            bitmapUrl = FileProvider.getUriForFile(mContext, mContext.getPackageName() + ".provider", bitmapFile);
+            File mediaStorageDir = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"IMG"
+            );
+
+            Log.d("gggg" , mediaStorageDir.getPath() + File.separator);
+            Log.d("gggg" , item.getItemImage());
+
+            File bitmapFile = new File(mediaStorageDir.getPath() + File.separator + item.getItemImage());
+
+            bitmapUrl = FileProvider.getUriForFile(mContext, mContext.getPackageName() + ".provider", bitmapFile);
+
+        } catch (Exception e) {
+            Log.d("file open error", e.toString());
+        }
+
+        itemView.setImageURI(bitmapUrl);
         // 経過日数
         TextView openPassed = (TextView) convertView.findViewById(R.id.position_open_passed);
         int diffDays = -1;
